ad5bad5609b27d3c8337254e5caf50491abc71f6
Medly13 <medly13@gmail.com>
2018-11-06 16:59:02 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1nnKsuH3bR9ClBM9_faF8S6WC9ve5M3StlA-uGA-FnN4/edit#gid=0';
